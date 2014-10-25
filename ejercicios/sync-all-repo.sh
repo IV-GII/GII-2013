023cca41270dac20fb0d951cfe0adaa03adb1e89
Ivan Ortega Alba <ivanortegaalba@gmail.com>
2014-10-25 17:42:48 +0200

    Añadidos archivos 10 y 11

--- ejercicios/sync-all-repo.sh
+++ ejercicios/sync-all-repo.sh
@@ -1,5 +1,4 @@
 #Este script esta referido con el alias sin-iv y sincroniza mi repositorio de iv con github.
 git add /Users/ivanortegaalba/Dropbox/UNIVERSIDAD_14-15/IV/GII-2014/*;
 git commit -m "$1";
-echo $1;
 git push
